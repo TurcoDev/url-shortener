42fcfc0c863993da3976ca959f9a998c6448b028
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2023-10-23 17:16:11 -0300

    add api fetch local

--- app/page.js
+++ app/page.js
@@ -1,7 +1,26 @@
+'use client'
 import Head from 'next/head'
 import styles from './styles/Home.module.css'
+import { useRef, useState } from 'react';
 
 export default function Home() {
+  const inputRef = useRef();
+  const [shortURL, setShortURL] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const url = inputRef.current.value;
+    
+    fetch('/api/shortUrl', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({url}),
+    })
+    .then(res => res.json())
+    .then(data => { setShortURL(data.shortUrl) });
+  }
 
   return (
     <div className={styles.container}>
@@ -21,12 +40,13 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <form className={styles.card}>
+          <form className={styles.card} onSubmit={handleSubmit}>
             <input type="text" className={styles.input} placeholder="URL" />
             <button className={styles.button}>Acorta</button>
-            <span className={styles.input}>**shortURL**</span>
+            <span className={styles.input}>{shortURL}</span>
           </form>
         </div>
+        
       </main>
     </div>
   )
